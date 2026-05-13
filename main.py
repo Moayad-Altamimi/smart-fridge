6a09370ecc6ed99caa2ee94a989d221c15c0f091
Moayad-Altamimi <moayadmaltamimi@gmail.com>
2026-05-14 00:06:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -132,5 +132,6 @@ def inventory_remove_used(payload: InventoryRemoveItems):
     return {"inventory": get_inventory()}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
