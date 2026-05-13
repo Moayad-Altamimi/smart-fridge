234d89155e192a84d9f209fa9876df879825a5aa
Moayad-Altamimi <moayadmaltamimi@gmail.com>
2026-05-14 00:14:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿from typing import List, Optional
+from typing import List, Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
